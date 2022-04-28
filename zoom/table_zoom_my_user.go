41747a7a013220a48dc8c168e2237eb768460e4b
Ved misra <47312748+misraved@users.noreply.github.com>
2022-04-28 19:19:33 +0530

    Recompile plugin with steampipe-plugin-sdk-v3.1.0 and go v1.18 (#14)

--- zoom/table_zoom_my_user.go
+++ zoom/table_zoom_my_user.go
@@ -5,8 +5,8 @@ import (
 
 	"github.com/himalayan-institute/zoom-lib-golang"
 
-	"github.com/turbot/steampipe-plugin-sdk/grpc/proto"
-	"github.com/turbot/steampipe-plugin-sdk/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/v3/grpc/proto"
+	"github.com/turbot/steampipe-plugin-sdk/v3/plugin"
 )
 
 func tableZoomMyUser(ctx context.Context) *plugin.Table {
